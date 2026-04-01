f0b16c9d952b047d961542ef6d87663572d4169f
Peijing <ngpeijing8@gmail.com>
2026-04-01 13:13:19 +1000

    uodate user login and regster form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Auth from './pages/Auth';
+import UserLoginRegister from './pages/UserLoginRegister';
 import AdminLogin from './pages/AdminLogin';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
@@ -12,8 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/register" element={<Auth />} />
+        <Route path="/login" element={<UserLoginRegister />} />
+        <Route path="/register" element={<UserLoginRegister />} />
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
